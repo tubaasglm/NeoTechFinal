e880db69937c2cb588547d7c7a7f3befd869019c
Tugba Saglam <48831456+tubaasglm@users.noreply.github.com>
2019-12-19 23:46:46 +0100

    ADD FURNITURE

---
+++ Assets/Scripts/introduction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class introduction : MonoBehaviour
+{
+    public GameObject panelIntro;
+
+    public void Continue()
+    {
+        Destroy(panelIntro);
+    }
+    public void OnTriggerStay(Collider other)
+    {
+        Cursor.visible = true;
+        if(panelIntro)
+        {
+            if(Input.GetKeyDown(KeyCode.L))
+            {
+                panelIntro.SetActive(true);
+            }
+        }
+        
+    }
+}
